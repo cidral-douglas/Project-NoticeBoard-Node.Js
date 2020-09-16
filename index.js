5f9ddba95f6460126e46a15897768825ad1ef17c
Douglas Cidral <doug.coding20@gmail.com>
2020-09-16 14:00:59 -0300

    Organizing the files

--- index.js
+++ index.js
@@ -1,35 +1,24 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const posts = require('./model/posts');
 
 const PORT = 3000;
 const app = express();
 
-let posts = [
-    {
-    id: "laknscik",
-    title: "Teste do Mural de Avisos",
-    description: "Descrição do teste"
-    },
-];
-
 app.get("/all", (req, res) => {
-    res.json(JSON.stringify(posts));
+    res.json(JSON.stringify(posts.getAll()));
 });
 
 app.post("/new", bodyParser.json(), (req, res) => {
 
-    let id = generateId();
     let title = req.body.title;
     let description = req.body.description;
 
-    posts.push({id, title, description});
+   posts.newPost( title, description);
     
     res.send("Post adicionado");
 });
 
-function generateId() {
-    return Math.random().toString(36).substr(2,9);
-}
 
 app.listen(PORT, () => {
     console.log("Servidor rodando na porta ", PORT);
