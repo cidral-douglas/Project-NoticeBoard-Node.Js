58479a3421da8efd061ee6a90dd4f3cd269636ee
Douglas Cidral <doug.coding20@gmail.com>
2020-09-16 13:48:31 -0300

    Adding the POST request

--- index.js
+++ index.js
@@ -15,7 +15,21 @@ let posts = [
 app.get("/all", (req, res) => {
     res.json(JSON.stringify(posts));
 });
-app.post("/new", (req, res) => {});
+
+app.post("/new", bodyParser.json(), (req, res) => {
+
+    let id = generateId();
+    let title = req.body.title;
+    let description = req.body.description;
+
+    posts.push({id, title, description});
+    
+    res.send("Post adicionado");
+});
+
+function generateId() {
+    return Math.random().toString(36).substr(2,9);
+}
 
 app.listen(PORT, () => {
     console.log("Servidor rodando na porta ", PORT);
